0c6505452e923312a4a065308f23d0566f12fbf0
rnin9 <rkdalswn1209@kw.ac.kr>
2021-06-07 17:14:47 +0900

    making business Logic in service

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,8 @@
+//실제는 datebase 모델을 만들어야 함.
+
+export class Movie{
+    id: number;
+    title:string;
+    year: number;
+    genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,40 +1,41 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { ppid } from 'process';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies') // entrypoint 변경으로 routing 초기설정가능
 export class MoviesController {
-
+    constructor(readonly MoviesService: MoviesService) { }
     @Get()
-    getAll(): string {
-        return 'hi movie'
+    getAll(): Movie[] {
+        return this.MoviesService.getAll();
     }
     @Get('search')
-    search(@Query("name") movieID){         // url resources 가져오기 = @Query
+    search(@Query("name") movieID) {         // url resources 가져오기 = @Query
         return `I'm searching ${movieID}`;
     }
 
     @Get(':id') // 이쪽 값과 @Param 값은 동일해야 한다.
-    getOne(@Param('id') movieId: string): string{ 
-        return `this is ${movieId}`;
+    getOne(@Param('id') movieID: string): Movie {
+        return this.MoviesService.getOne(movieID);
     }
 
     @Post()
-    create(@Body() reqBody){
-        return `create movie with ${reqBody}`;
+    create(@Body() reqBody) {
+        return this.MoviesService.create(reqBody);
     }
 
     @Delete('/:id')
-    remove(@Param('id') movieID:string): string{
-        return `delete movie ${movieID}`;
+    remove(@Param('id') movieID: string) {
+        return this.MoviesService.deleteOne(movieID);
     }
 
     @Patch(':/id')
-    updateOne(@Param('id') movieID:string, @Body() reqBody){    // 값 가져올떄는 @Param @Body로 body 데이터 가져오기 가능.
+    updateOne(@Param('id') movieID: string, @Body() reqBody) {    // 값 가져올떄는 @Param @Body로 body 데이터 가져오기 가능.
         return {
-         updateMovie: movieID,
-         ...reqBody //스프레드 연산자.
-        }     
+            updateMovie: movieID,
+            ...reqBody //스프레드 연산자.
+        }
     }
 
-   
+
 }
